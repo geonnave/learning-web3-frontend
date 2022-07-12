ce55c7b93e97113a4ec88c015ed1bb397b287bb0
Geovane Fedrecheski <geonnave@gmail.com>
2022-07-12 19:59:14 -0300

    feat: connect works

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { ethers } from "ethers";
+import logo from './logo.svg'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [isConnected, setIsConnected] = useState({selectedAddress: undefined});
+
+  let connectToMetamask = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.send("eth_requestAccounts", []);
+    setIsConnected({ selectedAddress: accounts[0] });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello React + Ethereum!</p>
+        <p>
+          {/* <button type="button" onClick={() => setCount((count) => count + 1)}> */}
+          <button type="button" onClick={connectToMetamask}>
+            Connect to Metamask: {isConnected.selectedAddress !== undefined ? "connected" : "disconneted"}
+          </button>
+        </p>
+      </header>
+      <main>
+        <p>
+          Content goes here.
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export default App
